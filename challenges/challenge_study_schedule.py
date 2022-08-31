be1750b1383362e183d2ee9dded9a810a488ff2a
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-31 20:25:59 -0300

    feat add function study_chalenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if type(target_time) is not int or target_time < 0:
+        return None
+    count = 0
+    for period in permanence_period:
+        if type(period[0]) is not int or type(period[1]) is not int:
+            return None
+        if (target_time >= period[0]  and target_time <= period[1]):
+            count += 1
+    return count
