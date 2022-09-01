e1777e43a8dd3c111e859fbeb80c05da5ed77511
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-31 23:20:42 -0300

    feat: add function is palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+
+    if word == "":
+        return False
+    
+    if (word[low_index] != word[high_index]):
+        return False
+
+    if len(word) <= 2:
+        return True
+
+    wd = word[1:-1]
+    return is_palindrome_recursive(wd, 0, len(wd)-1)
